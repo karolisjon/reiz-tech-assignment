3e667962f6c868e8635c67629b7073945a663b91
karolisjon <karolisjonn@gmail.com>
2023-01-17 17:54:46 +0200

    feat: used useState and useEffect hooks to fetch data and to console log it for now

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Typography } from "@mui/material";
 
 const App = () => {
+const [countries, setCountries] = useState([]);
+
+useEffect(() => {
+  const fetchCountries = async () => {
+    const response = await fetch('https://restcountries.com/v3.1/all');
+    const data = await response.json();
+
+    setCountries(data);
+  }
+
+  fetchCountries();
+}, []);
+
+console.log('countries', countries);
+
   return (
     <Typography>test</Typography>
   );
