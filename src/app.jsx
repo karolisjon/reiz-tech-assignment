6befd4995843e62a6b4302cc424bd43d9a0e93c6
karolisjon <karolisjonn@gmail.com>
2023-01-17 17:37:16 +0200

    feat: created initial custom MUI theme and imported a custom font, wrapped App component with a ThemeProvider

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Typography } from "@mui/material";
+
+const App = () => {
+  return (
+    <Typography>test</Typography>
+  );
+}
+
+export default App;
---
+++ src/styles/theme.js
@@ -0,0 +1,13 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Quicksand',
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700,
+  }
+});
+
+export default theme;
